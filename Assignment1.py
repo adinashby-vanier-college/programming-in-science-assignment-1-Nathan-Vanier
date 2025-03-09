1faffa6e563ca333cb7378763a5f8d5efee1515d
Nathan-Vanier <nathan@alertepi.com>
2025-03-09 18:57:52 +0000

    Finished Q1-4

--- Assignment1.py
+++ Assignment1.py
@@ -1,26 +1,42 @@
 # Function 1: Using Python built-in functions
 # This function should take three numbers as input and return their max.
 def built_in_functions_max(num1, num2, num3):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    numbers = [num1, num2, num3]
+    return max(numbers)
 
 # Function 2: Using Python built-in functions
 # This function should take three numbers as input and return their min.
 def built_in_functions_min(num1, num2, num3):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    numbers = [num1, num2, num3]
+    return min(numbers)
 
 # Function 3: Conditional Statements – The If Statement
 # This function should check if a number is positive, negative, or zero and return the corresponding message.
 def check_number(number):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    result = ""
+    if number > 0:
+        result += "Positive"
+    elif number < 0:
+        result += "Negative"
+    else:
+        result += "Zero"
+    return(result)
+
+
 
 # Function 4: For Loop – Making a Star Shape
 # This function should return a string representing a star shape.
 def star_shape(rows):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    result = ""
+    for i in range(1, rows + 1):
+        for j in range(1, i + 1):
+            result += "*"
+        result += "\n"
+    return result.rstrip()
+ 
+
+
+
 
 # Function 5: While Loop – Counting Multiples of 3
 # This function should return a list of numbers from 1 to limit, replacing multiples of 3 with "Multiple of 3".
